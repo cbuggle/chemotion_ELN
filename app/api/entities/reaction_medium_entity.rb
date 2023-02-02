7a38fb713155737cc3d94175632bd2fdde2c39fd
Christian Buggle <github@buggle.net>
2023-02-02 14:50:29 +0100

    Add short_label, preferred_label to ReactionMedium
    Add short_label, preferred_label aliasing label to ReactionMedium for UI convenience.

--- app/api/entities/reaction_medium_entity.rb
+++ app/api/entities/reaction_medium_entity.rb
@@ -4,7 +4,10 @@ module Entities
   # wraps a ReactionsSample object
   class ReactionMediumEntity < ApplicationEntity
     expose(
-      :id, :sum_formula, :label, :sample_name, :molecule_name
+      :id, :sum_formula, :label, :short_label, :preferred_label, :sample_name, :molecule_name
     )
+
+    # :label, :short_label, :preferred_label for compatibitility with SampleEntity.
+    # We would need probably only one of them.
   end
 end
