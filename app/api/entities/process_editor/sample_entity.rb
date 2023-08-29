0e1ab143f8a84ff46b671960f769a7c2d19ec2a2
Christian Buggle <christian@buggle.net>
2023-08-29 18:23:33 +0200

    Add amount attributes to ProcessEditor::SampleEntity
    Expose `:amount_mmol`, `:amount_mg`, `:amount_ml` attributes in ProcessEditor::SampleEntity to allow unit conversion in frontend.

--- app/api/entities/process_editor/sample_entity.rb
+++ app/api/entities/process_editor/sample_entity.rb
@@ -8,6 +8,11 @@ module Entities
       expose :sample_svg_file
       expose :target_amount_unit
       expose :target_amount_value
+      expose :amount_mmol
+      expose :amount_mg
+      expose :amount_g
+      expose :amount_ml
+      expose :metrics
       expose :location
       expose :hide_in_eln
     end
