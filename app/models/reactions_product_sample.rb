91448f0b735afee64f1ba90fb6985992dac5ee47
Christian Buggle <christian@buggle.net>
2023-11-28 11:57:48 +0100

    Add models for ReactionProcessEditor
    * Add backend models for ReactionProcessEditor, i.e. ReactionProcess, ReactionProcessStep, ReactionProcessActivity, Provenance, MediumSamples.
    * Enhance existing models where required.
    * Add Specs for ReactionProcess
    
    * Add Specs, Factories ReactionProcess, ReactionProcessStep, ReactionProcessActivity
    * Add gem ‘test-prof’ for `create_default` strategy substantially simplifying associations setup in FactoryBot.
    
    * Lots of rubocop changes in affected files.
    * Rake annotate all.

--- app/models/reactions_product_sample.rb
+++ app/models/reactions_product_sample.rb
@@ -2,20 +2,24 @@
 #
 # Table name: reactions_samples
 #
-#  id                      :integer          not null, primary key
-#  reaction_id             :integer
-#  sample_id               :integer
-#  reference               :boolean
-#  equivalent              :float
-#  position                :integer
-#  type                    :string
-#  deleted_at              :datetime
-#  waste                   :boolean          default(FALSE)
-#  coefficient             :float            default(1.0)
-#  show_label              :boolean          default(FALSE), not null
-#  gas_type                :integer          default("off")
-#  gas_phase_data          :jsonb
-#  conversion_rate         :float
+#  id                           :integer          not null, primary key
+#  reaction_id                  :integer
+#  sample_id                    :integer
+#  reference                    :boolean
+#  equivalent                   :float
+#  position                     :integer
+#  type                         :string
+#  deleted_at                   :datetime
+#  waste                        :boolean          default(FALSE)
+#  coefficient                  :float            default(1.0)
+#  show_label                   :boolean          default(FALSE), not null
+#  reaction_process_activity_id :uuid
+#  intermediate_type            :string
+#  created_at                   :datetime
+#  updated_at                   :datetime
+#  gas_type                     :integer          default("off")
+#  gas_phase_data               :jsonb
+#  conversion_rate              :float
 #
 # Indexes
 #
