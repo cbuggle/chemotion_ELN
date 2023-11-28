95077a7d25bee7e9e50cdc765e53cb929d17b18c
Christian Buggle <christian@buggle.net>
2023-11-28 11:57:48 +0100

    Add models for ReactionProcessEditor
    * Add backend models for ReactionProcessEditor, i.e. ReactionProcess, ReactionProcessStep, ReactionProcessActivity, Provenance, MediumSamples.
    * Enhance existing models where required
    * Add Specs for ReactionProcess
    
    * Add Specs, Factories ReactionProcess, ReactionProcessStep, ReactionProcessActivity
    * Add gem ‘test-prof’ for `create_default` strategy substantially simplifying associations setup in FactoryBot.
    
    * Lots of rubocop changes in affected files.

---
+++ app/models/reactions_intermediate_sample.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: reactions_samples
+#
+#  id                :integer          not null, primary key
+#  reaction_id       :integer
+#  sample_id         :integer
+#  reference         :boolean
+#  equivalent        :float
+#  position          :integer
+#  type              :string
+#  deleted_at        :datetime
+#  waste             :boolean          default(FALSE)
+#  coefficient       :float            default(1.0)
+#  show_label        :boolean          default(FALSE), not null
+#  reaction_step     :integer
+#  intermediate_type :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime
+#
+# Indexes
+#
+#  index_reactions_samples_on_reaction_id  (reaction_id)
+#  index_reactions_samples_on_sample_id    (sample_id)
+#
+
+class ReactionsIntermediateSample < ReactionsSample
+  # STI: this file is required for rails model autoloading.
+  # place all code in app/models/reactions_sample.rb.
+end
