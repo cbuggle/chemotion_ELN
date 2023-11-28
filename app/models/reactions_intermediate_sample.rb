625b41f026063daa12ac03ac5b9ae2741d7df972
Christian Buggle <christian@buggle.net>
2023-11-28 11:57:48 +0100

    Add models for ReactionProcessEditor
    * Add backend models for ReactionProcessEditor, i.e. ReactionProcess, ReactionProcessStep, ReactionProcessActivity, Provenance, MediumSamples.
    * Enhance existing models where required.
    * Add Specs for ReactionProcess
    
    * Add Specs, Factories ReactionProcess, ReactionProcessStep, ReactionProcessActivity
    * Add gem ‘test-prof’ for `create_default` strategy substantially simplifying associations setup in FactoryBot.
    
    * Lots of rubocop changes in affected files.
    * Rake annotate all.

---
+++ app/models/reactions_intermediate_sample.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: reactions_samples
+#
+#  id                           :integer          not null, primary key
+#  coefficient                  :float            default(1.0)
+#  conversion_rate              :float
+#  deleted_at                   :datetime
+#  equivalent                   :float
+#  gas_phase_data               :jsonb
+#  gas_type                     :integer          default("off")
+#  intermediate_type            :string
+#  position                     :integer
+#  reference                    :boolean
+#  show_label                   :boolean          default(FALSE), not null
+#  type                         :string
+#  waste                        :boolean          default(FALSE)
+#  weight_percentage            :float
+#  weight_percentage_reference  :boolean          default(FALSE)
+#  created_at                   :datetime
+#  updated_at                   :datetime
+#  reaction_id                  :integer
+#  reaction_process_activity_id :uuid
+#  sample_id                    :integer
+#
+# Indexes
+#
+#  index_reactions_samples_on_reaction_id  (reaction_id)
+#  index_reactions_samples_on_sample_id    (sample_id)
+#
+
+class ReactionsIntermediateSample < ReactionsSample
+  # STI: this file is required for rails model autoloading.
+  # place all code in app/models/reactions_sample.rb.
+end
