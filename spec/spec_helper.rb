b632c9824b20bfb02edde899dc6ef0e0a43d91e2
Christian Buggle <christian@buggle.net>
2023-11-28 11:57:48 +0100

    Add models for ReactionProcessEditor
    * Add backend models for ReactionProcessEditor, i.e. ReactionProcess, ReactionProcessStep, ReactionProcessActivity, Provenance, MediumSamples.
    * Enhance existing models where required
    * Add Specs for ReactionProcess
    
    * Add Specs, Factories ReactionProcess, ReactionProcessStep, ReactionProcessActivity
    * Add gem ‘test-prof’ for `create_default` strategy substantially simplifying associations setup in FactoryBot.
    
    * Lots of rubocop changes in affected files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/repeat'
 require 'webmock/rspec'
 
 require 'factory_bot_rails'
+require 'test_prof/recipes/rspec/factory_default'
 require 'faker'
 require 'capybara'
 require 'webdrivers'
@@ -17,7 +18,7 @@ require 'rails_helper'
 Capybara.register_driver :selenium do |app|
   http_client = Selenium::WebDriver::Remote::Http::Default.new(
     open_timeout: nil,
-    read_timeout: 500
+    read_timeout: 500,
   )
 
   options = Selenium::WebDriver::Chrome::Options.new
@@ -32,16 +33,16 @@ Capybara.register_driver :selenium do |app|
       browser: 'ALL',
       client: 'ALL',
       driver: 'ALL',
-      server: 'ALL'
-    }
+      server: 'ALL',
+    },
   )
 
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
     http_client: http_client,
-    options: options
-    #desired_capabilities: capabilities
+    options: options,
+    # desired_capabilities: capabilities
   )
 end
 
@@ -86,26 +87,30 @@ RSpec.configure do |config|
         .to_return(
           status: 200,
           body: File.read(
-            Rails.root + "spec/fixtures/body_#{target}.json"
+            Rails.root + "spec/fixtures/body_#{target}.json",
           ),
-          headers: { 'Content-Type' => 'application/json' }
+          headers: { 'Content-Type' => 'application/json' },
         )
     end
 
     stub_request(:get, 'http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/UFWIBTONFRDIAS-UHFFFAOYSA-N/record/JSON')
-      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type' => 'text/json' })
+      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                       'Content-Type' => 'text/json' })
       .to_return(status: 200, body: '', headers: {})
 
     stub_request(:get, 'http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/YMWUJEATGCHHMB-UHFFFAOYSA-N/record/JSON')
-      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type' => 'text/json' })
+      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                       'Content-Type' => 'text/json' })
       .to_return(status: 200, body: '', headers: {})
 
     stub_request(:get, 'http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/UHOVQNZJYSORNB-UHFFFAOYSA-N/record/JSON')
-      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type' => 'text/json' })
+      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                       'Content-Type' => 'text/json' })
       .to_return(status: 200, body: '', headers: {})
 
     stub_request(:get, 'http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/PNNRZXFUPQQZSO-UHFFFAOYSA-N/record/JSON')
-      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type' => 'text/json' })
+      .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                       'Content-Type' => 'text/json' })
       .to_return(status: 200, body: '', headers: {})
 
     stub_request(:post, 'http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/record/JSON')
@@ -113,7 +118,7 @@ RSpec.configure do |config|
       .to_return(
         status: 200,
         body: File.read(Rails.root + 'spec/fixtures/body_two_compounds.json'),
-        headers: { 'Content-Type' => 'application/json' }
+        headers: { 'Content-Type' => 'application/json' },
       )
     stub_request(:get, %r{http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/\S+/xrefs/RN/JSON})
       .with(headers: { 'Content-Type' => 'text/json' })
@@ -130,7 +135,7 @@ RSpec.configure do |config|
       .to_return(
         status: 200,
         body: File.read(Rails.root + 'spec/fixtures/body_123456789_CAS.xml'),
-        headers: { 'Content-Type' => 'application/xml' }
+        headers: { 'Content-Type' => 'application/xml' },
       )
     stub_request(:get, %r{http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/\S+/cids/TXT}).
       # with(:headers => {'Content-Type'=>'text/json'}).
@@ -142,7 +147,7 @@ RSpec.configure do |config|
       .to_return(
         status: 200,
         body: File.read(Rails.root + 'spec/fixtures/body_643785_LCSS.json'),
-        headers: { 'Content-Type' => 'application/json' }
+        headers: { 'Content-Type' => 'application/json' },
       )
     stub_request(:get, 'https://www.alfa.com/en/catalog/A14672')
       .with(headers: { 'Accept' => '*/*', 'Access-Control-Request-Method' => 'GET', 'User-Agent' => 'Google Chrome' })
