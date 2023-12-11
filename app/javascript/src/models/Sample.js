9d3a3bc308d0c23a688da8fe7120100c79dee739
Christian Buggle <christian@buggle.net>
2023-12-12 00:54:48 +0100

    Add javascript for ReactionProcessEditor data in ELN UI.
    * Add ReactionProcessEditor to ReactionDetails, ReactionDetailsScheme, Material, MaterialGroup.
    * Add ReactionEditorLink a link targeting the ReactionProcessEditor.
    
    Code almost exclusively courtesy of @adambasha0.
    
    Fix ReactionDetailsScheme rebase defect

--- app/javascript/src/models/Sample.js
+++ app/javascript/src/models/Sample.js
@@ -1059,7 +1059,7 @@ export default class Sample extends Element {
     }
 
     if (this.amount_unit === 'mol' && (this.isGas()
-    || this.isFeedstock())) return this.amount_value;
+      || this.isFeedstock())) return this.amount_value;
 
     return this.convertGramToUnit(this.amount_g, 'mol');
   }
@@ -1449,7 +1449,7 @@ export default class Sample extends Element {
 
   get molecule_iupac_name() {
     return this.molecule_name_hash && this.molecule_name_hash.label
-        || this.molecule && this.molecule.iupac_name;
+      || this.molecule && this.molecule.iupac_name;
   }
 
   set molecule_iupac_name(iupac_name) {
@@ -1608,6 +1608,22 @@ export default class Sample extends Element {
       return 1;
     }
     return this._equivalent;
+  }
+
+  get intermediate_type() {
+    return this._intermediate_type;
+  }
+
+  set intermediate_type(intermediate_type) {
+    this._intermediate_type = intermediate_type;
+  }
+
+  get reaction_step() {
+    return this._reaction_step;
+  }
+
+  set reaction_step(reaction_step) {
+    this._reaction_step = reaction_step;
   }
 
   set conc(conc) {
@@ -1797,7 +1813,9 @@ export default class Sample extends Element {
         ? this.components.map((s) => s.serializeComponent())
         : [],
       weight_percentage_reference: this.weight_percentage_reference || false,
-      weight_percentage: this.weight_percentage
+      weight_percentage: this.weight_percentage,
+      intermediate_type: this.intermediate_type,
+      reaction_step: this.reaction_step,
     };
     _.merge(params, extra_params);
     return params;
@@ -1888,7 +1906,7 @@ export default class Sample extends Element {
       // Filter by chemical identity (inchikey or smiles) rather than label
       // to avoid duplicates when the same compound has different labels (e.g., "Acetone" vs "propan-2-one")
       const filtered = tmpSolvents.find((solv) => (solv && solv.smiles === solventData.smiles
-            && solv.inchikey === solventData.inchikey));
+        && solv.inchikey === solventData.inchikey));
       if (!filtered) {
         tmpSolvents.push(solventData);
       } else if (newSolvent.external_label) {
@@ -1909,7 +1927,7 @@ export default class Sample extends Element {
     // Match by chemical identity (inchikey or smiles) rather than label
     // to ensure deletion works even if labels differ (e.g., "Acetone" vs "propan-2-one")
     const filteredIndex = tmpSolvents.findIndex((solv) => (solv.smiles === solventToDelete.smiles
-            && solv.inchikey === solventToDelete.inchikey));
+      && solv.inchikey === solventToDelete.inchikey));
     if (filteredIndex >= 0) {
       tmpSolvents.splice(filteredIndex, 1);
     }
@@ -1926,7 +1944,7 @@ export default class Sample extends Element {
     }
 
     const filteredIndex = tmpSolvents.findIndex((solv) => (solv.smiles === solventToUpdate.smiles
-              && solv.inchikey && solventToUpdate.inchikey));
+      && solv.inchikey && solventToUpdate.inchikey));
     if (filteredIndex >= 0) {
       tmpSolvents[filteredIndex] = solventToUpdate;
 
