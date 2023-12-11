6b4df7cd90fb2be7c861d5e1f23f1ace902f1091
Christian Buggle <christian@buggle.net>
2023-12-12 00:30:54 +0100

    Add API for ReactionProcessEditor
    * Add API endpoints required for ReactionProcessEditor, mount via Grape::API.
    * Add sign_in, sign_out endpoints to routes.rb, SessionsController to handle ReactionProcessEditor API authentication.
    * Add jti_token_authentication strategy to api.rb.
    * Add Entities to present ReactionProcessEditor data, slightly enhance ReactionEntity et al.
    * Add usecases for ReactionProcessEditor domain logic.
    * Add required unit conversions to unit_convertable.rb.
    * Add specs for ReactionProcessEditor API endpoints, shared_example ‘authenticated API call’.
    * Add constant HOSTNAME_REACTION_PROCESS_EDITOR to .env.
    
    *  Auto-update schema annotations, Auto-lint in several model classes.

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+# config/initializers/cors.rb:
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '/api/*', headers: :any, methods: %i[get post patch put delete],
+                       expose: %w[Authorization Content-Disposition Content-Filename]
+  end
+end
