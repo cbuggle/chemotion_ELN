5c918e42f97541ce11eab4075041e7c98799a9a2
Christian Buggle <christian@buggle.net>
2023-12-12 00:54:48 +0100

    Add javascript for ReactionProcessEditor data in ELN UI.
    * Add ReactionProcessEditor to ReactionDetails, ReactionDetailsScheme, Material, MaterialGroup.
    * Add ReactionEditorLink a link targeting the ReactionProcessEditor.
    
    Code almost exclusively courtesy of @adambasha0.
    
    Fix ReactionDetailsScheme rebase defect

--- app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/ReactionDetails.js
@@ -79,6 +79,7 @@ const productLink = (product) => (
   </span>
 );
 
+import ReactionEditorLink from 'src/components/reaction_editor/ReactionEditorLink';
 export default class ReactionDetails extends Component {
   constructor(props) {
     super(props);
@@ -433,6 +434,7 @@ export default class ReactionDetails extends Component {
                       <i className="fa fa-floppy-o " />
                     </Button>
                   </OverlayTrigger>
+                <ReactionEditorLink reaction={reaction} />
                 </>
               )}
             {copyBtn}
