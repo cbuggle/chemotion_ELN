37a066c1e39d0210983e62905783ed1ca8149148
Christian Buggle <christian@buggle.net>
2024-02-29 21:23:42 +0100

    Expose step_names in ReactionProcessVesselEntity
    * Expose step_names in ReactionProcessVesselEntity, evaluate from all reaction_process_steps with same vessel.

--- app/api/entities/reaction_process_editor/reaction_process_vessel_entity.rb
+++ app/api/entities/reaction_process_editor/reaction_process_vessel_entity.rb
@@ -6,11 +6,16 @@ module Entities
       expose(:id, :preparations)
 
       expose :vessel, using: 'Entities::ReactionProcessEditor::VesselEntity'
+      expose :step_names
 
       private
 
       def preparations
         object.preparations || []
+      end
+
+      def step_names
+        object.reaction_process.reaction_process_steps.where(vessel_id: object.vessel.id).order(&:position).map(&:name)
       end
     end
   end
