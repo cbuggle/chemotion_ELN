accab8bede8464f3131932cb36dba12e1029c5be
Christian Buggle <christian@buggle.net>
2024-03-08 14:57:57 +0100

    Reinsert SweepUnused

--- app/usecases/reaction_process_editor/reaction_process_vessels/sweep_unused.rb
+++ app/usecases/reaction_process_editor/reaction_process_vessels/sweep_unused.rb
@@ -5,22 +5,22 @@ module Usecases
     module ReactionProcessVessels
       class SweepUnused
         def self.execute!(reaction_process_id:)
-          # persisted_vessel_ids = ::ReactionProcessEditor::ReactionProcessVessel
-          #                        .where(reaction_process_id: reaction_process_id).pluck(:id)
+          persisted_vessel_ids = ::ReactionProcessEditor::ReactionProcessVessel
+                                 .where(reaction_process_id: reaction_process_id).pluck(:id)
 
-          # current_steps = ::ReactionProcessEditor::ReactionProcessStep.where(reaction_process_id: reaction_process_id)
-          # current_activities = ::ReactionProcessEditor::ReactionProcessActivity.where(
-          #   reaction_process_step_id: current_steps.pluck(:id),
-          # )
+          current_steps = ::ReactionProcessEditor::ReactionProcessStep.where(reaction_process_id: reaction_process_id)
+          current_activities = ::ReactionProcessEditor::ReactionProcessActivity.where(
+            reaction_process_step_id: current_steps.pluck(:id),
+          )
 
-          # current_step_vessel_ids = current_steps.pluck(:reaction_process_vessel_id).uniq
-          # current_activity_vessel_ids = current_activities.pluck(:reaction_process_vessel_id).compact.uniq
+          current_step_vessel_ids = current_steps.pluck(:reaction_process_vessel_id).uniq
+          current_activity_vessel_ids = current_activities.pluck(:reaction_process_vessel_id).compact.uniq
 
-          # current_vessel_ids = (current_step_vessel_ids + current_activity_vessel_ids).uniq
+          current_vessel_ids = (current_step_vessel_ids + current_activity_vessel_ids).uniq
 
-          # obsolete_vessel_ids = persisted_vessel_ids - current_vessel_ids
+          obsolete_vessel_ids = persisted_vessel_ids - current_vessel_ids
 
-          # ::ReactionProcessEditor::ReactionProcessVessel.where(id: obsolete_vessel_ids).destroy_all
+          ::ReactionProcessEditor::ReactionProcessVessel.where(id: obsolete_vessel_ids).destroy_all
         end
       end
     end
