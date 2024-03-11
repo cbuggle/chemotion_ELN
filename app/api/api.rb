20bc8dff4bb6367fe091121c00e5ec479e5e06f3
Christian Buggle <christian@buggle.net>
2024-03-11 18:05:58 +0100

    Vessel Preparations (RPE#2)
    * Add ReactionProcessVessels
    
    * Add model, migration for ReactionProcessVessel.
    * Add associations in Vessel, ReactionProcess, ReactionProcessStep, ReactionProcessActivity.
    * Add API to update ReactionProcessVessels.
    ** Allow param[:reaction_process_vessel] in API endpoint `PUT /reaction_process_steps`, replacing obsolete API endpoint `reaction_process_step/:id/vessel`.
    ** Add ReactionProcessVesselEntity, include reaction_process_vessel(s) in ReactionProcessEntity, ReactionProcessStepEntity, ReactionProcessActivityEntity.
    ** Expose step_names in ReactionProcessVesselEntity, evaluate from all reaction_process_steps with same vessel.
    
    * Add ORD Export
    ** Add attribute vessel to ReactionProcessAction in ORD schema and ReactionAction::Base exporter.
    ** Add ReactionProcessVesselExporter (replacing VesselExporter) to export Vessel with its preparations, etc. (as they only exist in the context of a ReactionProcess).
    ** Add ReactionProcessVesselExporterVesselPreparationsExporter.
    ** Add ORD constants `vessel_preparation_types`to SelectOptions.
    
    * Add UseCaseses
    ** Add ReactionProcessVessel::CreateOrUpdate to handle ReactionProcessVessel.preparations editing.
    ** Execute ReactionProcessVessels::CreateOrUpdate in POST, PUT /process_step API, POST/PUT activity.
    ** Add Usecase ReactionProcessVessels::SweepUnused to keep list of currently used vessels up to date.
    ** Adapt Usecase ReactionProcessActivity::Update (was: UpdateWorkup) to extract and manage ReactionProcessVessel params when updating Activities.
    
    * Fix pluralization of folder names in `spec/reaction_process/editor/.
    
    # Conflicts:
    #	app/api/reaction_process_editor/reaction_process_api.rb
    #	app/api/reaction_process_editor/reaction_process_step_api.rb
    #	app/ord_kit/proto/ord_kit_reaction_pb.rb
    #	spec/api/reaction_process_editor/reaction_process_api/post_reaction_process_steps_spec.rb
    
    # Conflicts:
    #	app/api/reaction_process_editor/reaction_process_activity_api.rb
    #	app/models/reaction_process_editor/samples_preparation.rb
    #	config/initializers/cors.rb
    
    # Conflicts:
    #	app/ord_kit/proto/ord_kit_reaction.proto
    #	app/ord_kit/proto/ord_kit_reaction_pb.rb

--- app/api/api.rb
+++ app/api/api.rb
@@ -224,6 +224,7 @@ class API < Grape::API
     mount ::ReactionProcessEditor::ReactionProcessAPI
     mount ::ReactionProcessEditor::ReactionProcessActivityAPI
     mount ::ReactionProcessEditor::ReactionProcessStepAPI
+    mount ::ReactionProcessEditor::ReactionProcessVesselAPI
     mount ::ReactionProcessEditor::VesselAPI
   end
 
