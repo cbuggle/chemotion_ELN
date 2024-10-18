0fc4c56dda769a1f14c7e7feadf10618c01370ec
Christian Buggle <christian@buggle.net>
2024-10-18 18:43:09 +0200

    Add SFTP sync for devices files

--- app/jobs/synchronize_automation_devices_files.rb
+++ app/jobs/synchronize_automation_devices_files.rb
@@ -3,11 +3,9 @@
 # Job to sync with an SFTP server
 # Downloads Devices CSV files defining the Devices currently available in the automation lab.
 
-# module ReactionProcessEditor
 class SynchronizeAutomationDevicesFiles < ApplicationJob
   def perform
     Rails.logger.info("SynchronizeAutomationDevicesFiles running at #{Time.zone.now}")
     Usecases::ReactionProcessEditor::SFTP::SynchronizeDevices.execute!
   end
 end
-# end
