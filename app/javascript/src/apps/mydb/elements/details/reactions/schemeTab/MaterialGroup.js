a353dd2f1acfdd3508811cbf43dac3a8eb22f2c6
Adam Basha <55552142+adambasha0@users.noreply.github.com>
2024-11-15 21:28:47 +0100

    fix: Handling of saved Samples from Reaction Process Editor (RPE#12)
    * Fix MaterialGroup.js, Material.js to handle editing saved samples (amounts, intermediate_type).
    * Remove obsolete code and es-lint comments in Material.js
    
    * Adapt sample handling to ELN specific proprietary metrics data format.
    ** Add SampleAmountsConverter to convert to and from ELN format.
    ** Enhance SaveIntermediate to store amounts in ELN format.
    ** Add Usecase UpdateSampleAmountsInWorkup to synchronize RPE activity workup with changed sample data.
    ** Remove IntermediateType from Activity Entity (Frontend detects it from sample).
    ** Catch nil sample in SelectOptions::ReactionProcessStep to handle samples that have been deleted, e.g. from within the ELN.
    
    * Fix position when appending ReactionProcessActivity.
    
    * Eliminate duplicate code for `create` vs. `update` ReactionsSample in UpdateMaterials#associate_sample_with_reaction,
    ** Remove pointless if-else-branch.
    ** Fixes rubocop violation “Metrics/MethodLength”.
    
    Co-authored-by: Christian Buggle <christian@buggle.net>

--- app/javascript/src/apps/mydb/elements/details/reactions/schemeTab/MaterialGroup.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/schemeTab/MaterialGroup.js
@@ -360,7 +360,7 @@ function GeneralMaterialGroup({
   );
 
   const intermediatesTable = (
-    <table width="100%" className="reaction-scheme">
+    <table className="w-100 m-2">
       <colgroup>
         <col style={{ width: '4%' }} />
         <col style={{ width: '16%' }} />
@@ -382,9 +382,7 @@ function GeneralMaterialGroup({
           <th> </th>
         </tr>
       </thead>
-      <tbody>
-        {contents.map(item => item)}
-      </tbody>
+        {contents}
     </table>
   );
 
