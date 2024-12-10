b677cb2d41fb57591e6ec7ce9245199ea23ea9a8
Christian Buggle <christian@buggle.net>
2024-12-10 22:56:43 +0100

    Fix stationary_phase

--- app/api/entities/reaction_process_editor/select_options/models/device_methods.rb
+++ app/api/entities/reaction_process_editor/select_options/models/device_methods.rb
@@ -8,11 +8,10 @@ module Entities
           def select_options_for(device_methods)
             device_methods.map do |method|
               method.attributes
-                    .slice(*%w[label device_name detectors description steps default_inject_volume
+                    .slice(*%w[label device_name detectors description steps default_inject_volume stationary_phase
                                active])
                     .merge({ value: method.label,
-                             mobile_phase: options_for(method.mobile_phase),
-                             stationary_phase: method.stationary_phase })
+                             mobile_phase: options_for(method.mobile_phase) })
             end
           end
         end
