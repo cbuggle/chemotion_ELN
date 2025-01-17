60b08ebaab8d4d2d455ee78c296ca91e0c1d07e9
Christian Buggle <christian@buggle.net>
2025-01-17 07:44:32 +0100

    Rename Ontologies ontology_id (RPE#18)
    * Rename Ontologies ontology_Id (was: chmo_id).
    * Minor variable rename

--- db/schema.rb
+++ db/schema.rb
@@ -1087,7 +1087,7 @@ ActiveRecord::Schema.define(version: 2026_03_30_152941) do
   end
 
   create_table "ontologies", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "chmo_id"
+    t.string "ontology_id"
     t.string "device_code"
     t.string "name"
     t.string "label"
