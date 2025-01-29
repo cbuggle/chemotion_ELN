c14a9fb9738b39a0fbdd8ef131e4780b2f4df5ca
Christian Buggle <christian@buggle.net>
2025-01-29 10:39:24 +0530

    Fix order of ontologies
    * Order Ontologies by :updated_at (not: :ontology_id. Particularly affects order of automation_modes).

--- app/api/entities/reaction_process_editor/select_options/models/ontologies.rb
+++ app/api/entities/reaction_process_editor/select_options/models/ontologies.rb
@@ -6,11 +6,12 @@ module Entities
       module Models
         class Ontologies < Base
           def all
-            ::ReactionProcessEditor::Ontology.includes([:device_methods]).order(:ontology_id).map do |ontology|
+            ::ReactionProcessEditor::Ontology.includes([:device_methods]).order(:updated_at).map do |ontology|
               ontology.attributes
                       .slice(*%w[label ontology_id solvents link roles active])
                       .merge({
                                value: ontology.ontology_id,
+                               inactive: !ontology.active,
                                methods: SelectOptions::Models::DeviceMethods.new.select_options_for(
                                  ontology.device_methods,
                                ),
