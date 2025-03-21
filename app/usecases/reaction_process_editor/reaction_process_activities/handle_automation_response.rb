7105e0e92c6de054c1eef994823ac190c780615d
Christian Buggle <christian@buggle.net>
2025-03-21 18:10:16 +0530

    Remove dev-code in handleAutomateResponse

--- app/usecases/reaction_process_editor/reaction_process_activities/handle_automation_response.rb
+++ app/usecases/reaction_process_editor/reaction_process_activities/handle_automation_response.rb
@@ -6,16 +6,14 @@ module Usecases
       class HandleAutomationResponse
         def self.execute!(activity:, response_csv:)
           ActiveRecord::Base.transaction do
-            Rails.logger.info("response_csv:")
-            Rails.logger.info(response_csv)
             csv = CSV.parse(response_csv, headers: true, return_headers: false, col_sep: ';')
 
             row = csv[0]
 
-            length = 3
-
             tray_type = row[0]
-            vials = (row[1..row.length] * 6).map{ |value| value&.to_i}.in_groups_of(6 * length)
+            # TODO: Establish length (& width) from tray_type definitions.
+            length = 5
+            vials = (row[1..row.length]).map{ |value| value&.to_i}.in_groups_of(length)
 
             activity.automation_response = { tray_type: tray_type, vials: vials}.stringify_keys
 
