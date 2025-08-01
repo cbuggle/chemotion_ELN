b1aae0b754c519541519ab246b3c0a8dc12b113e
Christian Buggle <christian@buggle.net>
2025-08-01 23:35:12 +0200

    remove comments

--- app/usecases/reaction_process_editor/reaction_process_activities/assign_fraction_for_remove.rb
+++ app/usecases/reaction_process_editor/reaction_process_activities/assign_fraction_for_remove.rb
@@ -5,19 +5,12 @@ module Usecases
     module ReactionProcessActivities
       class AssignFractionForRemove
         def self.execute!(activity:)
-          Rails.logger.info('AssignFractionForRemove')
-          Rails.logger.info(activity)
-
           return unless activity.activity_name == 'REMOVE' && activity.workup['origin_type'] == 'SOLVENT_FROM_FRACTION'
 
           fraction_id = activity.workup['samples'][0]['id']
           fraction = ::ReactionProcessEditor::Fraction.find_by(id: fraction_id)
 
-          Rails.logger.info('Updating Fraction')
-          Rails.logger.info(fraction)
-
           fraction&.update(consuming_activity: activity)
-          Rails.logger.info(fraction)
           fraction
         end
       end
