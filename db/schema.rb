0426626342809dbfcbcc412d3c2ddde32c87cc9b
Christian Buggle <christian@buggle.net>
2025-08-03 19:39:41 +0200

    Introduce automation ordinal (RPE#34)
    Introduce `automation ordinal` to provide a unique (in the scope of a reaction) integer id for each Activity. This is required for automation devices in the lab that cannot handle long uuids.

--- db/schema.rb
+++ db/schema.rb
@@ -1014,6 +1014,7 @@ ActiveRecord::Schema.define(version: 2026_04_20_075649) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "stationary_phase", array: true
+    t.string "ontology_type"
   end
 
   create_table "ontology_device_methods", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1158,6 +1159,7 @@ ActiveRecord::Schema.define(version: 2026_04_20_075649) do
     t.datetime "deleted_at"
     t.uuid "reaction_process_vessel_id"
     t.jsonb "automation_response"
+    t.integer "automation_ordinal"
   end
 
   create_table "reaction_process_defaults", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1176,7 +1178,8 @@ ActiveRecord::Schema.define(version: 2026_04_20_075649) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
-    t.string "automation_status"
+    t.string "automation_mode"
+    t.jsonb "automation_control"
   end
 
   create_table "reaction_process_vessels", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1187,6 +1190,7 @@ ActiveRecord::Schema.define(version: 2026_04_20_075649) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
     t.string "vesselable_type"
+    t.string "cleanup"
   end
 
   create_table "reaction_processes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -1278,10 +1282,8 @@ ActiveRecord::Schema.define(version: 2026_04_20_075649) do
     t.integer "gas_type", default: 0
     t.jsonb "gas_phase_data", default: {"time"=>{"unit"=>"h", "value"=>nil}, "temperature"=>{"unit"=>"°C", "value"=>nil}, "turnover_number"=>nil, "part_per_million"=>nil, "turnover_frequency"=>{"unit"=>"TON/h", "value"=>nil}}
     t.float "conversion_rate"
-    t.uuid "reaction_process_activity_id"
-    t.string "intermediate_type"
-    t.datetime "created_at", precision: 6
-    t.datetime "updated_at", precision: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.jsonb "log_data"
     t.boolean "weight_percentage_reference", default: false
     t.float "weight_percentage"
