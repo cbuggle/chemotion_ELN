55f6c10116ccf91982d8591c00837d872e614246
Christian Buggle <christian@buggle.net>
2025-12-02 21:47:42 +0100

    comments

--- app/api/entities/reaction_process_editor/select_options/models/vessels.rb
+++ app/api/entities/reaction_process_editor/select_options/models/vessels.rb
@@ -10,13 +10,13 @@ module Entities
           end
 
           def preparation_types
+            # Subset of OrdKit::VesselPreparation::VesselPreparationType.constants
             titlecase_options_for ['OVEN_DRIED', 'FLAME_DRIED', 'EVACUATED_BACKFILLED', 'PURGED', 'CUSTOM', 'NONE']
-            # OrdKit::VesselPreparation::VesselPreparationType.constants
           end
 
           def cleanup_types
             titlecase_options_for ['WASTE', 'REMOVE', 'STORAGE']
-            # OrdKit::VesselCleanup::VesselCleanupType.constants
+            # Subset of OrdKit::VesselCleanup::VesselCleanupType.constants
           end
         end
       end
