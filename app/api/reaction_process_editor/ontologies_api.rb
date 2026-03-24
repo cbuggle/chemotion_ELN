6106f01412730c335a2dc61274441221bef67c85
Christian Buggle <christian@buggle.net>
2026-03-24 21:31:32 +0100

    OntologyEditor Api fixes

--- app/api/reaction_process_editor/ontologies_api.rb
+++ app/api/reaction_process_editor/ontologies_api.rb
@@ -9,14 +9,11 @@ module ReactionProcessEditor
 
     desc 'get Ontologies'
     get :ontologies do
-      { ontologies: ReactionProcessEditor::Ontology.where.not(label: nil) }
-      { ontologies: ReactionProcessEditor::Ontology.where(id: ["1fbee868-06d0-4981-bb21-494e6ec3f94f", "dc211332-c58b-46f8-8b93-f636d88ef265", "805912b7-3c00-4590-aa26-438b6d74c37f"]) }
-      { ontologies: ReactionProcessEditor::Ontology.where(id: ["805912b7-3c00-4590-aa26-438b6d74c37f"]) }
+      { ontologies: ReactionProcessEditor::Ontology.all.order(:ontology_id) }
     end
 
     params do
       requires :ontology, type: Hash do
-        optional :id
         requires :active, type: Boolean
         requires :ontology_id
         requires :label
@@ -30,11 +27,6 @@ module ReactionProcessEditor
     end
     desc 'Create or Update an Ontology'
     post :ontologies do
-
-      Rails.logger.info("POST ONTOLOGIES")
-      Rails.logger.info(params)
-      Rails.logger.info(permitted_params)
-      # create or update ontology
       Usecases::ReactionProcessEditor::Ontology::CreateOrUpdate.execute!(ontology_params: permitted_params[:ontology])
     end
   end
--- app/usecases/reaction_process_editor/ontology/create_or_update.rb
+++ app/usecases/reaction_process_editor/ontology/create_or_update.rb
@@ -5,7 +5,7 @@ module Usecases
     module Ontology
       class CreateOrUpdate
         def self.execute!(ontology_params:)
-          ontology = ::ReactionProcessEditor::Ontology.find_or_initialize_by(id: ontology_params[:id])
+          ontology = ::ReactionProcessEditor::Ontology.find_or_initialize_by(ontology_id: ontology_params[:ontology_id])
 
           ontology.update(ontology_params)
         end
