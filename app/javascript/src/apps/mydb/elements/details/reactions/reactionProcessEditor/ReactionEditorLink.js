e30936c1c594af244abb71d51c77d25d54c8234c
Christian Buggle <christian@buggle.net>
2026-04-27 18:42:16 +0200

    Fix ReactionEditorLink
    * Fix ReactionEditorLink.
    ** Fix import path.
    ** Use Button (was Nav.Link).
    ** Add prop size.

--- app/javascript/src/apps/mydb/elements/details/reactions/reactionProcessEditor/ReactionEditorLink.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/reactionProcessEditor/ReactionEditorLink.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Button, Tooltip, OverlayTrigger, Nav } from 'react-bootstrap';
 
-const ReactionEditorLink = ({ reaction, size = "sm" }) => {
+const ReactionEditorLink = ({ reaction, size="sm" }) => {
 	return (
-		<OverlayTrigger
-			placement="bottom"
-			overlay={<Tooltip id="editReaction">Edit Reaction Process</Tooltip>}
-		>
-			<Button
-				size={size}
-				variant="secondary"
-				href={reaction.editor_link_target}
-				target={"_blank"}
+		<>
+			<OverlayTrigger
+				placement="bottom"
+				overlay={<Tooltip id="editReaction">Edit Reaction Process</Tooltip>}
 			>
-				<i className="fa fa-edit" />
-			</Button>
-		</OverlayTrigger>
+				<Button
+					size={size}
+					variant="secondary"
+					href={reaction.editor_link_target}
+					target={"_blank"}
+				>
+					<i className="fa fa-edit" />
+				</Button>
+			</OverlayTrigger>
+		</>
 	)
 }
 
