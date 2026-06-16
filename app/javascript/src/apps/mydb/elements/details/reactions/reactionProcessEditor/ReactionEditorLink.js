855b606a8bc58610744f8c1518333a4a6d1f5631
Christian Buggle <christian@buggle.net>
2026-06-16 16:39:16 +0200

    rubocop, comments, debug output

--- app/javascript/src/apps/mydb/elements/details/reactions/reactionProcessEditor/ReactionEditorLink.js
+++ app/javascript/src/apps/mydb/elements/details/reactions/reactionProcessEditor/ReactionEditorLink.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import { Button, Tooltip, OverlayTrigger, Nav } from 'react-bootstrap';
 
-const ReactionEditorLink = ({ reaction, size="sm" }) => {
+const ReactionEditorLink = ({ reaction, size = "sm" }) => {
 	return (
-		<>
-			<OverlayTrigger
-				placement="bottom"
-				overlay={<Tooltip id="editReaction">Edit Reaction Process</Tooltip>}
+		<OverlayTrigger
+			placement="bottom"
+			overlay={<Tooltip id="editReaction">Edit Reaction Process</Tooltip>}
+		>
+			<Button
+				size={size}
+				variant="secondary"
+				href={reaction.editor_link_target}
+				target={"_blank"}
 			>
-				<Button
-					size={size}
-					variant="secondary"
-					href={reaction.editor_link_target}
-					target={"_blank"}
-				>
-					<i className="fa fa-edit" />
-				</Button>
-			</OverlayTrigger>
-		</>
+				<i className="fa fa-edit" />
+			</Button>
+		</OverlayTrigger>
 	)
 }
 
