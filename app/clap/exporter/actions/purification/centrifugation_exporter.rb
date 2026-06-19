ad9b472d61b0398a6b2aef8fc59952913738d5a9
Christian Buggle <christian@buggle.net>
2026-06-19 21:24:00 +0200

    rubocop

--- app/clap/exporter/actions/purification/centrifugation_exporter.rb
+++ app/clap/exporter/actions/purification/centrifugation_exporter.rb
@@ -7,14 +7,16 @@ module Clap
         class CentrifugationExporter < Actions::Base
           private
 
+          # rubocop:disable Layout/MultilineMethodCallIndentation
           def action_type_attributes
             {
               centrifugation: ReactionProcessAction::ActionCentrifugation
-                .new({ pressure: Metrics::PressureExporter.new(workup['PRESSURE']).to_clap,
-                       temperature: Metrics::TemperatureExporter.new(workup['TEMPERATURE']).to_clap,
-                       speed: Metrics::MotionExporter.new(workup['SPEED']).to_clap }),
+                             .new({ pressure: Metrics::PressureExporter.new(workup['PRESSURE']).to_clap,
+                                    temperature: Metrics::TemperatureExporter.new(workup['TEMPERATURE']).to_clap,
+                                    speed: Metrics::MotionExporter.new(workup['SPEED']).to_clap }),
             }
           end
+          # rubocop:enable Layout/MultilineMethodCallIndentation
         end
       end
     end
