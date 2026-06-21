922bb59a0920d95e0aedd90fce267252e3155bb1
Christian Buggle <christian@buggle.net>
2026-06-21 21:04:25 +0200

    fix samplespreps find_or_create

--- app/usecases/reaction_process_editor/samples_preparations/find_or_create.rb
+++ app/usecases/reaction_process_editor/samples_preparations/find_or_create.rb
@@ -5,9 +5,10 @@ module Usecases
     module SamplesPreparations
       class FindOrCreate
         def self.execute!(reaction_process:, sample_preparation:)
-          preparation = reaction_process.samples_preparations.find_or_initialize_by(
-            sample_id: sample_preparation['sample_id'],
-          )
+          preparation = reaction_process.samples_preparations.find_by(id: sample_preparation['id']) ||
+                        reaction_process.samples_preparations.find_or_initialize_by(
+                          sample_id: sample_preparation['sample_id'],
+                        )
           preparation.update(sample_preparation.except(:id))
         end
       end
